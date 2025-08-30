53174a80401fed1031c1386218cde800f2563b8d
Luciano Fernandes <93296181+lucffernands@users.noreply.github.com>
2025-08-30 12:36:57 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css"; // importar CSS se você criar
+import "./index.css"; // se quiser aplicar algum CSS global
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
